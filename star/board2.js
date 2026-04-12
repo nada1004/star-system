0bcf9d72b9ad5f7e66fa69d705fa2b3dac7236cf
nada1004 <263307734+nada1004@users.noreply.github.com>
2026-04-13 08:47:11 +0900

    Add files via upload

--- star/board2.js
+++ star/board2.js
@@ -2312,7 +2312,6 @@ function _b2UpdateMainDisplay(playerName) {
       thumbnail.style.boxShadow = 'none';
     }
   });
-  return;
 
   if (mainBox) {
     mainBox.style.setProperty('--theme-glow', theme.glow);
