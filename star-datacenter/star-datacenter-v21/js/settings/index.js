acd84179004d6db681248a340733586e3151f788
nada1004 <263307734+nada1004@users.noreply.github.com>
2026-05-17 16:10:57 +0900

    Delete star-datacenter/star-datacenter-v21 directory

--- star-datacenter/star-datacenter-v21/js/settings/index.js
+++
@@ -1,4 +0,0 @@
-(function(){
-  window.SettingsModules = window.SettingsModules || {};
-  try{ window.SettingsModules.design && window.SettingsModules.design.init && window.SettingsModules.design.init(); }catch(e){}
-})();
